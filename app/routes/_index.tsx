492fed877c59eeca73d475fc937abb6180fecad0
kcapka <147353671+kcapka@users.noreply.github.com>
2024-10-16 10:59:43 -0700

    SEO

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -3,9 +3,36 @@ import { useState } from "react";
 import Wordle from "~/components/Wordle";
 
 export const meta: MetaFunction = () => {
+
+  const seoImg = '/images/wordle-preview.jpg';
+
   return [
     { title: "Wordle Clone" },
     { name: "description", content: "This site is a clone of the popular New York Times game, Wordle! However, as opposed to the original game, you can play this one as many times a day as you'd like!" },
+    {
+      property: 'og:description',
+      content: "This site is a clone of the popular New York Times game, Wordle! However, as opposed to the original game, you can play this one as many times a day as you'd like!",
+    },
+    {
+      property: 'og:image',
+      content: seoImg,
+    },
+    {
+      property: 'og:image:secure_url',
+      content: seoImg,
+    },
+    {
+      property: 'og:image:width',
+      content: '1200',
+    },
+    {
+      property: 'og:image:height',
+      content: '628',
+    },
+    {
+      property: 'og:type',
+      content: 'website',
+    }
   ];
 };
 
