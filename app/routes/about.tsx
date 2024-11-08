7c4fa28890f35b4aedf15aa7f2958427c12df853
kcapka <147353671+kcapka@users.noreply.github.com>
2024-11-08 10:14:35 -0800

    Placeholder pages

---
+++ app/routes/about.tsx
@@ -0,0 +1,11 @@
+
+
+
+export default function AboutPage() {
+
+    return (
+        <section className="h-[100svh] pt-[100px]">
+            <h1 className="text-4xl font-display text-white text-center">Coming soon!</h1>
+        </section>
+    )
+}
